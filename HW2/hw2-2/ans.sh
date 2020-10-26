76e7593d2d522403fe53f5edcb2907543e7bd70a
Pin-Hua Lai <185-phlai410277@users.noreply.git.cs.nctu.edu.tw>
2020-10-26 19:37:21 +0800

    2-2-3ok

--- HW2/hw2-2/ans.sh
+++ HW2/hw2-2/ans.sh
@@ -6,19 +6,23 @@ ctrl_c(){
 	exit
 }
 Main(){
-	Option=$(dialog --clear --cancel-label "Exit" --menu "System Info Panel" 20 50 10 \
-		1 "LOGIN RANK" 2 "PORT INFO" 3 "MOUNTPOINT INFO" \
-		4 "SAVE SYSTEM INFO" 5 "LOAD SYSTEM INFO" \
-		2>&1 >/dev/tty)
-	result=$?
-	if [ $result -eq 0 ]; then
-		Select "$Option"
-		Main
-	elif [ $result -eq 1 ]; then
-		echo "Exit"
-	elif [ $result -eq 255 ]; then
-		echo "Esc pressed" 1>&2
-	fi
+	while :
+	do {
+		Option=$(dialog --clear --cancel-label "Exit" --menu "System Info Panel" 20 50 10 \
+				1 "LOGIN RANK" 2 "PORT INFO" 3 "MOUNTPOINT INFO" \
+				4 "SAVE SYSTEM INFO" 5 "LOAD SYSTEM INFO" \
+				2>&1 >/dev/tty)
+		result=$?
+		if [ $result -eq 0 ]; then
+			Select "$Option"
+		elif [ $result -eq 1 ]; then
+			echo "Exit"
+			exit
+		elif [ $result -eq 255 ]; then
+			echo "Esc pressed" 1>&2
+			exit
+		fi
+	} done
 }
 Select(){
 	case $1 in
@@ -26,7 +30,7 @@ Select(){
 			LoginRank
 			;;
 		2)
-			echo "PORT INFO"
+			PortInfo
 			;;
 		3)
 			echo "MOUNTPOINT INFO"
@@ -46,6 +50,23 @@ LoginRank(){
 		awk ' BEGIN { printf("Rank\tName\tTimes\n") } { printf("%s\t%s\t%s\n", NR, $1, $2) } '
 		)
 	dialog --title "LoginRank" --msgbox "$Top5" 20 50
+}
+PortInfo(){
+	sost=$(sockstat -P tcp,udp | sed '1d' | awk '{printf("%s %s_%s\n", $3, $5, $6)}')
+	while :
+	do {
+		Option=$(dialog --cancel-label "Exit" --menu "PORT INFO(PID and Port)" 20 50 40 $sost 2>&1 >/dev/tty)
+		if [ -z $Option ]; then
+			break
+		elif [ $Option = "?" ]; then
+			continue
+		else
+			detail=$(ps -p ${Option} -o user,pid,ppid,stat,%cpu,%mem,command | sed '1d' | \
+				awk ' { printf("USER: %s\nPID: %s\nPPID: %s\nSTAT: %s\n%%CPU: %s\n%%MEM: %s\nCOMMAND: %s", $1, $2, $3, $4, $5, $6, $7) } '
+				)
+			dialog --title "Process Status: ${Option}" --msgbox "$detail" 20 50
+		fi
+	} done
 }
 
 Main
